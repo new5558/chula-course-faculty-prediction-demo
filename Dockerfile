0a25e961b109f9d004f31e051bb4d0296bc5be38
new5558 <new17353@gmail.com>
2021-12-06 20:39:10 +0700

    feat: add shap interpretation

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ COPY requirements.txt ./requirements.txt
 RUN pip install -r requirements.txt
 RUN pip install sentencepiece
 RUN pip install thai2transformers==0.1.2 --no-dependencies
+RUN pip install matplotlib==3.5.0
 ENTRYPOINT ["streamlit", "run"]
 CMD ["main.py"]
--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from transformers import TextClassificationPipeline, AutoModelForSequenceClassification, AutoTokenizer, PreTrainedTokenizer
 from thai2transformers.preprocess import process_transformers
 from tokenizers import Tokenizer, AddedToken
 import shap
 
 model_path = './model'
+txt = ""
 
 def st_shap(plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+    shap_html = f"<head>{shap.initjs()}</head><body>{plot}</body>"
     components.html(shap_html, height=height)
 
 def hash_tokenizer(_):
@@ -23,24 +25,33 @@ def load_pipeline():
     return pipeline
 
 
-title = "Chula Course Faculty prediction demo"
-st.title(title)
+def main():
+    title = "Chula Course Faculty prediction demo"
+    st.set_page_config(page_title=title, page_icon="💎", layout="wide")
+    _, center_column, c_ = st.columns((1, 2, 1))
+    center_column.title(title)
 
+    pipeline = load_pipeline()
+    form = center_column.form("my_form")
+    txt = form.text_area('Course description thai', 'บทบาทของเทคโนโลยีสารสนเทศในการแก้ปัญหาทางธุรกิจ แนวคิดและพื้นฐานเทคนิคของศาสตร์เทคโนโลยีสารสนเทศ การวางแผน การพัฒนาและการจัดการของระบบประยุกต์สารสนเทศโดยคอมพิวเตอร์')
+    submit = form.form_submit_button(label="Predict Faculty")
 
-txt = st.text_area('Text to analyze', 'บทบาทของเทคโนโลยีสารสนเทศในการแก้ปัญหาทางธุรกิจ แนวคิดและพื้นฐานเทคนิคของศาสตร์เทคโนโลยีสารสนเทศ การวางแผน การพัฒนาและการจัดการของระบบประยุกต์สารสนเทศโดยคอมพิวเตอร์')
-processed_text = process_transformers(txt)
+    if submit:
+        processed_text = process_transformers(txt)
 
-pipeline = load_pipeline()
+        prediction = pipeline(processed_text)
+        center_column.json(prediction)
+        
+        explainer = shap.Explainer(pipeline)
+        shap_values = None
+        with st.spinner(text="Interpreting model with SHAP. we don't have GPU so this will take a 1-2 minutes"):
+            shap_values = explainer([processed_text])
+        
+        st_shap(shap.plots.text(shap_values[0, :, 2], display = False))
 
-prediction = pipeline(processed_text)
-st.json(prediction)
+if __name__ == "__main__":
+    main()
 
-explainer = shap.Explainer(pipeline)
-
-with st.spinner(text="Interpreting model by SHAP. we don't have GPU so this will take a 1-2 minutes"):
-    shap_values = explainer([processed_text])
-    st_shap(shap.text_plot(shap_values[0, :, 2], matplotlib=True))
-st.success()
 
 # st.text(shap_values)
 
